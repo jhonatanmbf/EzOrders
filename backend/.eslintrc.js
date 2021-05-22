d3f5c778f7d73e8efd81355ec71b90117e1ce5f4
jhonatanmbf <jmbf.jhonatan@gmail.com>
2021-05-22 01:16:58 -0300

    Create the Backend

---
+++ backend/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'linebreak-style': [0, ' error ', ' windows '],
+    'class-methods-use-this': 'off',
+    'consistent-return': 'off',
+    'no-console': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+mongoose.connect('mongodb://localhost:27017/ezorders', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+const app = express();
+const server = http.Server(app);
+const io = socketIo(server);
+
+app.use((request, response, next) => {
+  request.io = io;
+  return next();
+});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3001, () => console.log('🔥 Server Started at localhost:3001 🔥'));
